72d258438f6b7eccf233b3db853c13a96f61c7cb
Andrey Khokhlov <Hohlov908@gmail.com>
2022-11-05 22:50:43 +0300

    in progress

--- libs/game/src/lib/game-logic/scene/knights-game-scene.ts
+++ libs/game/src/lib/game-logic/scene/knights-game-scene.ts
@@ -10,8 +10,17 @@ export class KnightsGameScene extends Phaser.Scene {
   }
   preload() {
     console.log('preload method');
+    // Load in images and sprites
+    this.load.spritesheet('playerSprite', 'assets/sprites/player/playerSprite.png',
+      { frameWidth: 66, frameHeight: 60 }
+    ); // Made by tokkatrain: https://tokkatrain.itch.io/top-down-basic-set
+    //this.load.image('bullet', 'assets/sprites/bullets/bullet6.png');
+    //this.load.image('target', 'assets/demoscene/ball.png');
+    //this.load.image('background', 'assets/skies/underwater1.png');
   }
   create() {
     console.log('create method');
+    const player = this.physics.add.sprite(800, 600, 'playerSprite');
+    player.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true).setDrag(500, 500);
   }
 }
